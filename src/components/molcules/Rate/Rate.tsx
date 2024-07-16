fca5b0b9d06f60bf191ddad8cb037334831e752b
kotaro-777 <kou.aaa36@icloud.com>
2024-07-17 08:03:13 +0900

    fix: カスタムフックから関数実行のロジックを切り出す

--- src/components/molcules/Rate/Rate.tsx
+++ src/components/molcules/Rate/Rate.tsx
@@ -3,22 +3,33 @@ import { StarFilled, Star } from "@/components/atoms/Icon";
 import { useRating } from "@/hooks/useRating";
 
 type RateProps = {
+  /** ５段階評価の初期設定値 */
   initialRating: number;
+  /** 評価が変更された時に実行される関数 */
   onRatingChange?: (rating: number) => void;
 };
 
+/**
+ * ５段階評価を星で表現するコンポーネント
+ * ・ 5 が最高評価
+ */
 const Rate: FC<RateProps> = ({ initialRating, onRatingChange }) => {
-  const { rating, totalStars, handleClick } = useRating(
-    initialRating,
-    onRatingChange
-  );
+  const { rating, TOTAL_STARTS, selectRating } = useRating(initialRating);
+
+  const handleRatingClick = (newRating: number) => {
+    selectRating(newRating);
+
+    if (onRatingChange) {
+      onRatingChange(rating);
+    }
+  };
 
   return (
     <div className="flex">
-      {Array.from({ length: totalStars }).map((_, index) => {
+      {Array.from({ length: TOTAL_STARTS }).map((_, index) => {
         const starNumber = index + 1;
         return (
-          <span key={starNumber} onClick={() => handleClick(starNumber)}>
+          <span key={starNumber} onClick={() => handleRatingClick(starNumber)}>
             {starNumber <= rating ? <StarFilled /> : <Star />}
           </span>
         );
--- src/hooks/useRating.ts
+++ src/hooks/useRating.ts
@@ -1,22 +1,26 @@
 import { useState } from "react";
 
 export const useRating = (
-  initialRating: number,
-  onRatingChange?: (rating: number) => void
+  /** ５段階評価の初期設定値 */
+  initialRating: number
 ) => {
   const [rating, setRating] = useState(initialRating);
-  const totalStars = 5;
+  const TOTAL_STARTS = 5;
 
-  const handleClick = (newRating: number) => {
+  /**
+   * 変更後のレートをセットし、カスタムフックを実行する
+   * @param newRating 変更後のレート
+   */
+  const selectRating = (newRating: number) => {
     setRating(newRating);
-    if (onRatingChange) {
-      onRatingChange(newRating);
-    }
   };
 
   return {
+    /** 現在のレート */
     rating,
-    totalStars,
-    handleClick,
+    /** 評価の最大値 */
+    TOTAL_STARTS,
+    /** 変更後のレートをセットし、カスタムフックを実行する関数 */
+    selectRating,
   };
 };
