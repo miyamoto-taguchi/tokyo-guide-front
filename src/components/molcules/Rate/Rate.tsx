5e3a484dbf45cd98eb6074cde71d867316bad8d5
kotaro-777 <kou.aaa36@icloud.com>
2024-07-21 10:57:43 +0900

    fix: ホバー時にratingを変化させるように修正

--- src/components/molcules/Rate/Rate.tsx
+++ src/components/molcules/Rate/Rate.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { StarFilled, Star } from "@/components/atoms/Icon";
 import { useRating } from "@/hooks/useRating";
 
@@ -15,12 +15,12 @@ type RateProps = {
  */
 const Rate: FC<RateProps> = ({ initialRating, onRatingChange }) => {
   const { rating, TOTAL_STARTS, selectRating } = useRating(initialRating);
+  const [hoverRating, setHoverRating] = useState<number | null>(null);
 
   const handleRatingClick = (newRating: number) => {
     selectRating(newRating);
-
     if (onRatingChange) {
-      onRatingChange(rating);
+      onRatingChange(newRating);
     }
   };
 
@@ -29,8 +29,23 @@ const Rate: FC<RateProps> = ({ initialRating, onRatingChange }) => {
       {Array.from({ length: TOTAL_STARTS }).map((_, index) => {
         const starNumber = index + 1;
         return (
-          <span key={starNumber} onClick={() => handleRatingClick(starNumber)}>
-            {starNumber <= rating ? <StarFilled /> : <Star />}
+          <span
+            key={starNumber}
+            onClick={() => handleRatingClick(starNumber)}
+            onMouseEnter={() => setHoverRating(starNumber)}
+            onMouseLeave={() => setHoverRating(null)}
+          >
+            {hoverRating !== null ? (
+              starNumber <= hoverRating ? (
+                <StarFilled />
+              ) : (
+                <Star />
+              )
+            ) : starNumber <= rating ? (
+              <StarFilled />
+            ) : (
+              <Star />
+            )}
           </span>
         );
       })}
