74e9b62df3759c25051fc98983674a6217ce839a
Taguchi Hayato <>
2024-07-27 20:06:10 +0900

    feature/ログインフォームコンポーネントのストーリーのコメント修正

--- src/components/organisms/LoginForm/LoginForm.stories.tsx
+++ src/components/organisms/LoginForm/LoginForm.stories.tsx
@@ -2,30 +2,24 @@ import type { Meta, StoryObj } from "@storybook/react";
 import { fn } from "@storybook/test";
 import LoginForm from "./";
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+/**
+ * ログインフォームコンポーネントのストーリー
+ * organisms/LoginForm.tsx
+ */
 const meta = {
   title: "Example/LoginForm",
   component: LoginForm,
   parameters: {
-    // storybookの画面でのレイアウトオプション　centeredだと画面中央に配置される
-    // https://storybook.js.org/docs/configure/story-layout
     layout: "centered",
   },
-  //  自動でドキュメントを生成してくれるか
-  //  https://storybook.js.org/docs/writing-docs/autodocs
   tags: ["autodocs"],
-  //  プロップスに渡す値
-  //  https://storybook.js.org/docs/api/argtypes
   argTypes: {},
-  // https://storybook.js.org/docs/essentials/actions#action-args
   args: { onSubmit: fn() },
 } satisfies Meta<typeof LoginForm>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-// 各ストーリーのプロップスに渡す値
-// https://storybook.js.org/docs/writing-stories/args
 export const Default: Story = {
   args: {},
 };
